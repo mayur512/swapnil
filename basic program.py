ba8bb2070f3c8fee1ea22c2d56d19277b887fae4
Mayur Divekar <103174665+mayur512@users.noreply.github.com>
2022-04-10 10:04:08 +0530

    this is my first commit with original file

---
+++ basic program.py
@@ -0,0 +1,83 @@
+# print("my name is mayur")
+# print("i am 21 years old")
+
+
+#variables:
+# num=21        #integer variables
+# num1=67.9     #float variables
+# num2="hello"  #string variable
+# num3=True     #booelan variable
+# num4='m'      #character variables
+# print("value of num =",num)
+# print("value of num1 =",num1)
+# print("value of num2 =",num2)
+# print("value of num3 =",num3)
+# print("value of num4 =",num4)
+
+
+# print("value of num =",num,"\t\tvalue of num1 =",num1)
+
+# a="hello"
+# b="mayur"
+# print(a+b)
+# print(a+""+b)
+
+#type casting
+#int()- convert value to int
+# a="34"
+# b="56"
+# print((int(a))+(int(b)))
+
+#addition of 2 numbers:
+# num1=34
+# num2=78
+# sum=num1+num2
+# print("sum:",sum)
+
+
+#area of trangle:
+# base=32
+# ht=3
+# aot=0.5*base*ht
+# print("area of trangle:",aot)
+
+
+# a=int(input("enter value of a"))
+# b=int(input("enter value of b"))
+# c=a+b
+# print("addition:",c)
+
+
+# age=int(input("enter age"))
+# name=input("enter name")
+# city=input("enter city")
+# print("age\t\tname\t\tcity")
+# print(age,"\t\t",name,"\t\t",city)
+
+
+#simple interest
+
+
+
+# age=21
+# stud_name="mayur"
+# mark=89
+# city="pune"
+# print(f"age:-{age}\t stud_name:-{stud_name}\t mark:-{mark} city:-{city}")
+# print("age:",age,"stud_name:",stud_name,"mark:",mark)
+
+
+# print("hello",end=" ")
+# print("mayur")
+
+#how to take multiple values using single input function
+#a,b,c=input("enter values").split()
+#print(a,b,c)
+
+#addition
+# a,b,c=[int(i) for i in (input("enter values").split())]
+# print(a+b+c)
+
+#string slicing
+# mystr="mayur is good boy"
+# print(len(mystr))
